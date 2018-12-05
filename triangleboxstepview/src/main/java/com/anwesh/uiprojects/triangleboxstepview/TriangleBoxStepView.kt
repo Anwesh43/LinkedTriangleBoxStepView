bac230c77f12f08e40fca57b09e3e778f50e02d9
Anwesh43 <anweshthecool0@gmail.com>
2018-12-06 04:03:50 +0530

    adding libraries and defining nodes

--- triangleboxstepview/src/main/java/com/anwesh/uiprojects/triangleboxstepview/TriangleBoxStepView.kt
+++ triangleboxstepview/src/main/java/com/anwesh/uiprojects/triangleboxstepview/TriangleBoxStepView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.triangleboxstepview
 /**
  * Created by anweshmishra on 06/12/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 3
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
+val sizeFactor : Float = 2.3f
+val strokeFactor : Int = 50
+val color : Int = Color.parseColor("#0277BD")
