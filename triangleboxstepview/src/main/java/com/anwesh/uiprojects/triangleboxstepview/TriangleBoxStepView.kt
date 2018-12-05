8b9656a17a07d99db368750344ab2ab19c64d952
Anwesh43 <anweshthecool0@gmail.com>
2018-12-06 04:39:02 +0530

    adding Animator class

--- triangleboxstepview/src/main/java/com/anwesh/uiprojects/triangleboxstepview/TriangleBoxStepView.kt
+++ triangleboxstepview/src/main/java/com/anwesh/uiprojects/triangleboxstepview/TriangleBoxStepView.kt
@@ -95,4 +95,26 @@ class TriangleBoxStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                } catch (ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start(cb : () -> Unit) {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+    }
 }
