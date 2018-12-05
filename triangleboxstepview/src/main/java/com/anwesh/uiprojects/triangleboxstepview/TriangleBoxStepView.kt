40d872fdb3f9865159f21302551ddd2c4f09dca1
Anwesh43 <anweshthecool0@gmail.com>
2018-12-06 04:16:08 +0530

    adding Flooat and Int extension methods

--- triangleboxstepview/src/main/java/com/anwesh/uiprojects/triangleboxstepview/TriangleBoxStepView.kt
+++ triangleboxstepview/src/main/java/com/anwesh/uiprojects/triangleboxstepview/TriangleBoxStepView.kt
@@ -19,3 +19,13 @@ val scGap : Float = 0.05f
 val sizeFactor : Float = 2.3f
 val strokeFactor : Int = 50
 val color : Int = Color.parseColor("#0277BD")
+
+fun Int.getInverse() : Float = 1f / this
+
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.getInverse(), Math.max(0f, this - i * n.getInverse()))
+
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInverse() + scaleFactor() * b.getInverse()
+
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = scGap * dir * mirrorValue(a, b)
