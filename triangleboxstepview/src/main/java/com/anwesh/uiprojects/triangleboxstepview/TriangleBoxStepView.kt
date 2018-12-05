198242c481f07c0904e8c77c8210bfc2f72ef01f
Anwesh43 <anweshthecool0@gmail.com>
2018-12-06 04:49:58 +0530

    adding TriangleBoxStep class

--- triangleboxstepview/src/main/java/com/anwesh/uiprojects/triangleboxstepview/TriangleBoxStepView.kt
+++ triangleboxstepview/src/main/java/com/anwesh/uiprojects/triangleboxstepview/TriangleBoxStepView.kt
@@ -162,4 +162,27 @@ class TriangleBoxStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class TriangleBoxStep(var i : Int) {
+        private val root : TBSNode = TBSNode(0)
+        private var curr : TBSNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
